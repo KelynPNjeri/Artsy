28d466df4699059619e28cd6b3d72bb33f92a054
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-05-26 10:30:56 +0300

    Initial Commit

---
+++ app/src/main/java/io/musicalcoder/artsy/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.musicalcoder.artsy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.musicalcoder.artsy.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
